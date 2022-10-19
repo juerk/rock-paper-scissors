799bbaaf06b145e2a4a81c2d6378410f0310f966
juerk <wuerkerrs06@gmail.com>
2022-10-18 17:39:40 -0700

    Button Functionality is now working
    This is certainly not the most effecient way to write this code.. but it works!

--- script.js
+++ script.js
@@ -1,58 +1,104 @@
-let playerScore = 0;
 let computerScore = 0;
+let playerScore = 0;
 
-function computerPlay() {
-  let random = (Math.random() * 10)
-  if (random <= 3.33) {
-    return "ROCK"
-  } else if (random > 3.33 && random <= 6.66) {
-    return "SCISSORS"
+let rockButton = document.getElementById('rockButton');
+rockButton.addEventListener('click', () => {
+  playRoundRock();
+});
+
+let paperButton = document.getElementById('paperButton');
+paperButton.addEventListener('click', () => {
+  playRoundPaper();
+});
+
+let scissorsButton = document.getElementById('scissorsButton');
+scissorsButton.addEventListener("click", () => {
+  playRoundScissors();
+})
+
+
+const getComputerSelection = function(computerChoice) {
+  let random = Math.random();
+  if (random < .33) {
+    computerChoice = "ROCK"
+  } else if (random > .67) {
+    computerChoice = "PAPER"
   } else {
-    return "PAPER"
+    computerChoice = "SCISSORS"
   }
+  return computerChoice;
 }
 
-
-function playRound(computerSelection, playerChoice) {
-  if (
-    computerSelection === "ROCK" && playerChoice === "SCISSORS" 
-    || 
-    computerSelection === "SCISSORS" && playerChoice === "PAPER"
-    ||
-    computerSelection === "PAPER" && playerChoice === "ROCK"
-      ) {
-    computerScore += 1
-    return "COMPUTER WINS"
-  } else if (
-    playerChoice === "ROCK" && computerSelection === "SCISSORS"
-    ||
-    playerChoice === "SCISSORS" && computerSelection === "PAPER"
-    ||
-    playerChoice === "PAPER" && computerSelection === "ROCK"
-  ) {
-    playerScore += 1
-    return "PLAYER WINS"
-  } else {
-    return "DRAW"
+const playRoundRock = function(roundResult) {
+  let computerChoice = getComputerSelection();
+  if (computerChoice === "ROCK") {
+    roundResult = "DRAW"
+  } else if (computerChoice === "PAPER") {
+    roundResult = "Computer Wins!"
+    computerScore += 1;
+  } else if (computerChoice === "SCISSORS") {
+    roundResult = "Player Wins!"
+    playerScore += 1;
   }
-}
-
-for (let i = 0; i < 1000; i++) {
-  let playerChoice = prompt("What will it be, champ?").toUpperCase();
-  const computerSelection = computerPlay();
-  let roundResult = playRound(computerSelection, playerChoice);
-  console.log("Player: " + playerChoice)
-  console.log("Computer: " + computerSelection)
+  console.log(computerChoice);
   console.log(roundResult);
-  console.log("Player Score : " + playerScore);
-  console.log("Computer Score: " + computerScore);
-  console.log("")
+  console.log(playerScore);
+  console.log(computerScore);
+}
 
-  if (playerScore > 4 || computerScore > 4) {
-    console.log("GAME OVER")
-    break;
+const playRoundPaper = function(roundResult) {
+  let computerChoice = getComputerSelection();
+  if (computerChoice === "ROCK") {
+    roundResult = "Player Wins!"
+    playerScore += 1;
+  } else if (computerChoice === "PAPER") {
+    roundResult = "DRAW"
+  } else if (computerChoice === "SCISSORS") {
+    roundResult = "Computer Wins"
+    computerScore += 1;
   }
+  console.log(computerChoice);
+  console.log(roundResult);
+  console.log(playerScore);
+  console.log(computerScore);
+} 
+
+const playRoundScissors = function (roundResult) {
+  let computerChoice = getComputerSelection();
+  if (computerChoice === "ROCK") {
+    roundResult = "Computer Wins!"
+    computerScore += 1;
+  } else if (computerChoice = "PAPER") {
+    roundResult = "Players Wins!"
+    playerScore += 1;
+  } else if ( computerChoice = "SCISSORS") {
+    roundResult = "DRAW"
+  }
+  console.log(computerChoice);
+  console.log(roundResult);
+  console.log(playerScore);
+  console.log(computerScore);
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// computer Selection DONE
+// player Selection
+// connect buttons to player Selection
+// function to choose winner
+// keep player and computer score
+// first to 5 wins
+// output results to web screen
